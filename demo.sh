fec49a7cc5a591226dc8ea9bc1ced5781d4a1cf8
Mathieu Lecarme <mlecarme@scaleway.com>
2023-02-11 21:44:16 +0100

    Lets demo.

---
+++ demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -f debian.iso
+./bin/medusa-get debian.iso \
+    http://debian.koyanet.lv/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    http://debian.anexia.at/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    http://ftp.crifo.org/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    http://debian.obspm.fr/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    https://ftp.cica.es/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    http://ftp.ps.pl/pub/Linux/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    http://debian.mirror.root.lu/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    http://ftp.lanet.kr/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
+
