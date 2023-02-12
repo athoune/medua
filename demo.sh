780b2895ffd89061b939e8b5050d26d26ef92573
Mathieu Lecarme <mlecarme@scaleway.com>
2023-02-12 19:46:58 +0100

    feat: lazy shasum

--- demo.sh
+++ demo.sh
@@ -4,6 +4,11 @@ if [ ! -e debian.iso.wal ]
 then
 	rm -f debian.iso
 fi
+if [ ! -e SHA256SUMS ]
+then
+	curl -O https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA256SUMS
+fi
+
 ./bin/medusa-get debian.iso \
     http://debian.koyanet.lv/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
     http://debian.anexia.at/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
@@ -14,3 +19,5 @@ fi
     http://debian.mirror.root.lu/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
     http://ftp.lanet.kr/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
 
+shasum -a 256 debian.iso
+grep debian-11.6.0 SHA256SUMS
