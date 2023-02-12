550f2d4bbe06b8f309f72c3b35099e6a369341ae
Mathieu Lecarme <mlecarme@scaleway.com>
2023-02-12 22:43:37 +0100

    Test with a domain with a multiple A or AAAA

--- demo.sh
+++ demo.sh
@@ -17,7 +17,8 @@ fi
     https://ftp.cica.es/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
     http://ftp.ps.pl/pub/Linux/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
     http://debian.mirror.root.lu/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
-    http://ftp.lanet.kr/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
+    http://ftp.lanet.kr/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso \
+    https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
 
 shasum -a 256 debian.iso
 grep debian-11.6.0 SHA256SUMS
